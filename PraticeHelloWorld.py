360dbbe576d6d06b26ea6ba50c08e10363f45a35
Darniski <darniski@Darniskis-MacBook-Pro.local>
2020-01-21 15:08:34 -0500

    Practice Quiz: Hello World

---
+++ PraticeHelloWorld.py
@@ -0,0 +1,19 @@
+# Quiz 1 - 3 I wont put the choices just the Questions 
+
+# 1. What are functions in Python?
+
+# 2. What are keywords in Python?
+
+# 3. What does the print function do in Python?
+
+# Now for some actually code. 
+ 
+# 4. Output a message that says "Programming in Python is fun!" to the screen.
+print("Programming in Pyhthon is fun!")
+
+# 5. Replace the ___ placeholder and calculate the Golden ratio: 
+# Tip: to calculate the square root of a number x, you can use x**(1/2).
+ratio = (5**(1/2)+1)/2
+print(ratio)
+
+#Woohoo first module complete
