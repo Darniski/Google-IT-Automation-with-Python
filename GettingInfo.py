bd8cadb5bd66bc09ef407505c99ca2d8d892a856
Darniski <darniski@Darniskis-MacBook-Pro.local>
2020-01-21 14:13:21 -0500

    Getting info Excercise and solution.

---
+++ GettingInfo.py
@@ -0,0 +1,8 @@
+name = "Darnell"
+print("Hello " + name)
+#this is the example used in Getting Information
+
+color = "Yellow"
+thing = "sun flowers"
+print( color + " is the color of " + thing)
+#solution for the check point in the video
