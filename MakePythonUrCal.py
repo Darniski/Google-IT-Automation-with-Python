f31d73c4e28555e6cb85a467733cdcbcdd521660
Darniski <darniski@Darniskis-MacBook-Pro.local>
2020-01-21 14:28:22 -0500

    Make Python ur cal examples and solution.

---
+++ MakePythonUrCal.py
@@ -0,0 +1,14 @@
+print(4+5)
+
+print(9*7)
+
+print(-1/4)
+
+print(1/3) 
+
+print(((2050/5)-32)/9)
+
+print(2**10)
+# examples in the video ouput them in the console in vs code
+print((((1 + 2) * 3)/4)**5)
+# checkpoint solution
