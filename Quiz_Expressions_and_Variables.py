122085cef6f1f6625d3e96835b058341bfca4320
Darniski <darniski@Darniskis-MacBook-Pro.local>
2020-01-22 15:49:42 -0500

    Practice Quiz: Expressions and Variables

---
+++ Quiz_Expressions_and_Variables.py
@@ -0,0 +1,19 @@
+# This is a practice quiz: Expressions and Variables
+
+
+# 1. In this scenario, two friends are eating dinner at a restaurant. The bill comes in the amount of 47.28 dollars. The friends decide to split the bill evenly between them, after adding 15% tip for the service. Calculate the tip, the total amount to pay, and each friend's share, then output a message saying "Each person needs to pay: " followed by the resulting number.
+
+bill = 47.28
+tip = bill * 0.15
+total = bill + tip
+share = total / 2
+print("Each person needs to pay:" + str(share))
+
+# 2. This code is supposed to take two numbers, divide one by another so that the result is equal to 1, and display the result on the screen. Unfortunately, there is an error in the code. Find the error and fix it, so that the output is correct.
+
+numerator = 10
+denominator = 10
+result = numerator / denominator
+print(result)
+
+# Got a 100 woohoo :)
