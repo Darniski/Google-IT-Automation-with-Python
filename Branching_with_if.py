1fc8fd837a6c0e22d84a31dd3f127ab40f6685b7
Darniski <darniski@Darniskis-MacBook-Pro.local>
2020-01-22 21:29:10 -0500

    Branchinf with If and Else Statements

---
+++ Branching_with_if.py
@@ -0,0 +1,26 @@
+#example of username check for characters less then 3 video example
+def hint_username(username):
+    if len(username) < 3: 
+        print("Invalid username. Must be at least 3 characters long")
+    else: 
+        print("Valid username")
+
+# solution to video check 
+def is_positive(number):
+  if number > 0:
+    return True 
+  else:
+    return None
+
+# else statment video 
+def is_positive(number):
+  if number > 0:
+    return True 
+  else:
+    return False 
+
+#modulo 
+def is_even(number)
+     if number % 2 == 0:
+         return True
+    return False # even though the else statment isnt here and this will run and end depend on the out put 
