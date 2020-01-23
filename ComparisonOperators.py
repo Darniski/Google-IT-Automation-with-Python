5e0f10daf51d994474ee06f47131385c28baefa8
Darniski <darniski@Darniskis-MacBook-Pro.local>
2020-01-22 21:10:43 -0500

    Comparison Operators

---
+++ ComparisonOperators.py
@@ -0,0 +1,19 @@
+>>> print(10>1)
+True
+>>> print("cat" == "dog")
+False
+>>> print(1 != 2) 
+True
+>>> print( 1  < "1") # causes an error  
+#Traceback (most recent call last):
+# File "<stdin>", line 1, in <module>
+#TypeError: '<' not supported between instances of 'int' and 'str'
+print( 1  ==  "1")
+False
+>>> print("Yellow" > "Cyan" and "Brown" > "Magenta")
+False
+>>> print(25 > 50 or 1 != 2)
+True
+>>> print(not 42 == "Asnwer") 
+True
+>>> 
